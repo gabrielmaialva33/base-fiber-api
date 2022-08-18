6181f8bdc8e1c14a29567b14628ecc57697358af
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-08-18 01:05:14 -0300

    chore: configure logger

--- src/main.go
+++ src/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		EnableTrustedProxyCheck: true,
+		TrustedProxies:          []string{"0.0.0.0"},
+		ProxyHeader:             fiber.HeaderXForwardedFor,
+	})
+
+	app.Use(logger.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
