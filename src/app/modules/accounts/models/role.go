b81a80dd605c123fc89a333aa0fc8e224d21092a
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-10-30 00:21:08 -0300

    refactor: add field in role

--- src/app/modules/accounts/models/role.go
+++ src/app/modules/accounts/models/role.go
@@ -8,12 +8,13 @@ import (
 type Role struct {
 	gorm.Model
 
-	Id        string         `gorm:"primaryKey;default:uuid_generate_v4()" json:"id"`
-	Name      string         `gorm:"column:name;size:80;not null;unique;index;" json:"name" validate:"required,min=2,max=80,unique,omitempty"`
-	Slug      string         `gorm:"column:slug;size:80;not null;unique;" json:"slug" validate:"required,min=2,max=80,unique,omitempty"`
-	CreatedAt time.Time      `gorm:"column:created_at;not null;default:CURRENT_TIMESTAMP" json:"-"`
-	UpdatedAt time.Time      `gorm:"column:updated_at;not null;default:CURRENT_TIMESTAMP" json:"-"`
-	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at;index,default:null" json:"-"`
+	Id          string         `gorm:"primaryKey;default:uuid_generate_v4()" json:"id"`
+	Name        string         `gorm:"column:name;size:80;not null;unique;index;" json:"name" validate:"required,min=2,max=80,unique,omitempty"`
+	Slug        string         `gorm:"column:slug;size:80;not null;unique;" json:"slug" validate:"required,min=2,max=80,unique,omitempty"`
+	Description string         `gorm:"column:description;size:255;not null;" json:"description" validate:"required,min=2,max=255,omitempty"`
+	CreatedAt   time.Time      `gorm:"column:created_at;not null;default:CURRENT_TIMESTAMP" json:"-"`
+	UpdatedAt   time.Time      `gorm:"column:updated_at;not null;default:CURRENT_TIMESTAMP" json:"-"`
+	DeletedAt   gorm.DeletedAt `gorm:"column:deleted_at;index,default:null" json:"-"`
 
 	// Relations
 	Users []*User `gorm:"many2many:user_roles;" json:"users"`
@@ -22,14 +23,16 @@ type Role struct {
 type Roles []Role
 
 type RolePublic struct {
-	Id   string `json:"id"`
-	Slug string `json:"slug"`
+	Id          string `json:"id"`
+	Slug        string `json:"slug"`
+	Description string `json:"description"`
 }
 
 func (r *Role) PublicRole() interface{} {
 	return &RolePublic{
-		Id:   r.Id,
-		Slug: r.Slug,
+		Id:          r.Id,
+		Slug:        r.Slug,
+		Description: r.Description,
 	}
 }
 
